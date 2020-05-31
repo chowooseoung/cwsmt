8942731cda3319b7ba9a8a1d81bb85e004eadb98
chowooseoung <chowooseoung@gmail.com>
2020-06-01 01:18:30 +0900

    room dev

--- scripts/room/gui/main.py
+++ scripts/room/gui/main.py
@@ -1,55 +1,138 @@
+import sys
+sys.path.append("D:\maya\scripts")
+
 from PySide2 import QtWidgets, QtCore, QtGui, QtUiTools
-# from common.gui.uiloader import UiLoader
+from common.gui.uiloader import UiLoader
+from common.gui.mworkspacecontrol import MWorkspaceControl
+from shelfDelegate import ShelfDelegate, ShelfModel, ShelfItem
 
+import glob
+import json
 import os
-
-
-class UiLoader(QtUiTools.QUiLoader):
-
-    _baseinstance = None
-
-    def createWidget(self, classname, parent=None, name=''):
-        if parent is None and self._baseinstance is not None:
-            widget = self._baseinstance
-        else:
-            widget = super(UiLoader, self).createWidget(classname, parent, name)
-            if self._baseinstance is not None:
-                setattr(self._baseinstance, name, widget)
-
-        return widget
-    
-    def loadUi(self, uifile, baseinstance=None):
-        self._baseinstance = baseinstance
-        widget = self.load(uifile)
-        # QtCore.QMetaObject.connectSlotByName(widget)
-
-        return widget
 
 
 class RoomUI(QtWidgets.QDialog):
 
-    WINDOW_TITLE = 'Room'
-    UI = os.path.normpath(os.path.join(os.path.dirname(__file__), 'ui', 'main.ui'))
-    PREFERENCEUI = os.path.normpath(os.path.join(os.path.dirname(__file__), 'ui', 'preferenceButton.ui'))
+    windowTitle = 'Room' 
+    uiFile = os.path.normpath(os.path.join(os.path.dirname(__file__), 'ui', 'main.ui'))
 
-    def __init__(self):
+    preferenceJsonFile = os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir, '.room', 'preference.json'))
+
+    uiInstance = None
+
+    # maya workspace method # 
+    @classmethod
+    def display(cls, host):
+        if cls.uiInstance:
+            cls.uiInstance.show_workspace_control()
+        else:
+            cls.uiInstance = cls(host)
+
+    @classmethod
+    def get_workspace_control_name(cls):
+        return "{0}WorkspaceControl".format(cls.__name__)
+
+    @classmethod
+    def get_ui_script(cls):
+        ui_script = "from {0} import {1}\n{1}.display('maya')".format(cls.__module__, cls.__name__)
+        return ui_script
+
+    def create_workspace_control(self):
+        self.workspace_control_instance = MWorkspaceControl(self.get_workspace_control_name())
+        if self.workspace_control_instance.exists():
+            self.workspace_control_instance.restore(self)
+        else:
+            self.workspace_control_instance.create(self.__class__.windowTitle, self, ui_script=self.get_ui_script())
+
+    def show_workspace_control(self):
+        self.workspace_control_instance.set_visible(True) ## 
+
+    @property
+    def host(self):
+        return self.__host
+
+    @host.setter
+    def host(self, h):
+        self.__host = h
+
+    @property
+    def prefInitJson(self):
+        path = [[os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'maya')),
+                os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'maya', 'scripts')),
+                os.path.normpath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'maya', 'plug-ins'))]]
+        return self.create_pref_json(['maya'], path)
+
+    def create_pref_json(self, tools, path):
+        json = {}
+        json['tools'] = tools
+        for i in range(len(tools)):
+            json[tools[i]] = {'shelfPath' : path[i][0],
+                            'scriptsPath' : path[i][1],
+                            'pluginsPath' : path[i][2]}
+        return json
+
+    def __init__(self, host):
         super(RoomUI, self).__init__()
-        UiLoader().loadUi(uifile=self.UI, baseinstance=self)
-        
-        self.level = 0
-        self.create_preference()
+        UiLoader().loadUi(uifile=self.uiFile, baseinstance=self)
 
-    def create_preference(self):
-        for i in range(30):
-            preferenceButton = QtWidgets.QWidget()
-            UiLoader().loadUi(uifile=self.PREFERENCEUI, baseinstance=preferenceButton)
-            preferenceButton.resize(128, 128)
-            
-            item = QtWidgets.QListWidgetItem()
-            item.setSizeHint(QtCore.QSize(128, 128))
+        self.setObjectName(self.__class__.__name__)
 
-            self.listWidget.addItem(item)
-            self.listWidget.setItemWidget(item, preferenceButton)
+        self.create_workspace_control()
+
+        self.shelfModel = ShelfModel()
+        self.listView.setItemDelegate(ShelfDelegate())
+        self.listView.setModel(self.shelfModel)
+
+        self.host = host
+        if not os.path.exists(RoomUI.preferenceJsonFile):
+            self.dump_json(self.prefInitJson, RoomUI.preferenceJsonFile)
+        self.preferenceJson = self.load_json(RoomUI.preferenceJsonFile)
+
+        if self.host == 'maya':
+            import pymel.core as pm
+            userShelfDir = pm.system.internalVar(userShelfDir=True)
+            mayaJsonFIle = os.path.normpath(os.path.join(userShelfDir, 'room.mayaShelf.json'))
+            if os.path.exists(mayaJsonFIle):
+                mayaJson = self.load_json(mayaJsonFIle)
+                if mayaJson.has_key('shelf'):
+                    self.shelf = mayaJson['shelf']
+                else:
+                    self.shelf = None
+            else:
+                mayaJson = {}
+                self.dump_json({}, mayaJsonFIle)
+                self.shelf = None
+            self.userJson = mayaJson
+
+        self.init_listview()
+
+    def init_listview(self):
+        if not self.shelf:
+            shelfList = glob.glob(os.path.normpath(os.path.join(self.preferenceJson[self.host]['shelfPath'], '*.json')))
+            for i in range(len(shelfList)):
+                json = self.load_json(shelfList[i])
+                btn = QtWidgets.QToolButton()
+                btn.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+                btn.setIcon(QtGui.QIcon(json['icon']))
+                btn.setText(shelfList[i])
+                btn.setCheckable(True)
+                btn.setChecked(False)
+                btn.setSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+                item = ShelfItem(name=shelfList[i], runCode=None, toolTip=None, icon=None, label=shelfList[i], widget=btn)
+                self.shelfModel.appendRow(item)
+            print('init_listview')
+            return
+        else:
+            pass
+
+    def load_json(self, jsonFile):
+        with open(jsonFile, 'r') as f:
+            r = json.load(f)
+        return r
+
+    def dump_json(self, data, jsonFile):
+        with open(jsonFile, 'w') as f:
+            json.dump(data, f, indent=4)
 
     def showEvent(self, e):
         super(RoomUI, self).showEvent(e)
@@ -60,47 +143,20 @@ class RoomUI(QtWidgets.QDialog):
         super(RoomUI, self).closeEvent(e)
 
         print('closeEvent')
-    
+
     def hideEvent(self, e):
         super(RoomUI, self).hideEvent(e)
 
         print('hideEvent')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     """ Run the application. """
     from PySide2.QtWidgets import (QApplication, QTableWidget, QTableWidgetItem,
                                    QAbstractItemView)
-    import sys
-
-    sys.path.append('D:\\maya')
     app = QApplication(sys.argv)
 
-    test3ui = RoomUI()
+    test3ui = RoomUI('maya')
     test3ui.show()
-    # a = QtWidgets.QFrame()
-    # UiLoader().loadUi(uifile=os.path.normpath(os.path.join(os.path.dirname(__file__), 'ui', 'preferenceButton.ui')), baseinstance=a)
-    # a.show()
 
     sys.exit(app.exec_())
--- scripts/room/gui/shelfDelegate.py
+++ scripts/room/gui/shelfDelegate.py
@@ -1,25 +1,67 @@
-from PySide2 import QtWidgets
-from common.gui.uiloader as UiLoader
+import sys
+sys.path.append('D:\\maya\\scripts')
 
+from PySide2 import QtWidgets, QtCore, QtGui
+from common.gui.uiloader import UiLoader
+
+import json
 import os
+
+
+class ShelfItem(QtGui.QStandardItem):
+
+    @property
+    def widget(self):
+        return self.__widget
+
+    @widget.setter
+    def widget(self, w):
+        self.__widget = w
+
+    def __init__(self, name, runCode, toolTip, icon, label, widget):
+        super(ShelfItem, self).__init__()
+
+        self.__name = name
+        self.__runCode = runCode
+        self.__toolTip = toolTip
+        self.__icon = icon
+        self.__label = label
+        self.widget = widget
+
+    def set_data(self):
+        pass
+
+class ShelfModel(QtGui.QStandardItemModel):
+
+    def __init__(self):
+        super(ShelfModel, self).__init__()
 
 
 class ShelfDelegate(QtWidgets.QStyledItemDelegate):
 
-    PREFERENCEUI = os.path.normpath(os.path.join(os.path.dirname(__file__), 'ui', 'preferenceItem.ui'))
-    PARENTUI = os.path.normpath(os.path.join(os.path.dirname(__file__), 'ui', 'parentItem.ui'))
-
     def __init__(self, parent=None):
         super(ShelfDelegate, self).__init__(parent)
 
-        self.preferenceWidget = QtWidgets.QWidget()
-        UiLoader().loadUi(uifile=self.PREFERENCEUI, self.preferenceWidget)
-
-        self.parentWidget = QtWidgets.QWidget()
-        UiLoader().loadUi(uifile=self.PARENTUI, self.parentWidget)
+        # self.preferenceWidget = CustomFrame()
 
     def paint(self, painter, option, index):
-        pass
+        if index.row() == 0:
+            print('a')
+        else:
+            QtWidgets.QStyledItemDelegate.paint(self, painter, option, index)
 
     def sizeHint(self, option, index):
-        pass
+        return QtCore.QSize(32, 32)
+
+
+
+if __name__ == "__main__":
+    """ Run the application. """
+    from PySide2.QtWidgets import (QApplication, QTableWidget, QTableWidgetItem,
+                                   QAbstractItemView)
+    
+    app = QApplication(sys.argv)
+
+    print(sys.path)
+
+    sys.exit(app.exec_())
