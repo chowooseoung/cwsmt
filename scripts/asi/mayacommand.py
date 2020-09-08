c5f258d84061719c9aa814749e8d56c30bfb1436
cho wooseoung <chowooseoung@gmail.com>
2020-09-08 14:37:15 +0900

    git

--- scripts/asi/mayacommand.py
+++ scripts/asi/mayacommand.py
@@ -3,12 +3,12 @@
 import pymel.core as pm
 
 
-def add_shelf_button():
+def add_shelf_button(parent, image, command, doubleClickCommand, label, imageOverlayLabel, sourceType, annotation):
     current_tab = pm.tabLayout("ShelfLayout", query=True, selectTab=True)
 
     pm.shelfButton(parent=,
                     image=,
-                    command=,
+                    command=, 
                     doubleClickCommand=,
                     label=,
                     imageOverlayLabel=,
